a9d9def080fe710fcf11111b0cc3ae93833b0f8a
Carlos Eduardo <2005carloseduardosantos@gmail.com>
2023-08-03 14:44:18 -0300

    initial comit

---
+++ src/scripts/ScrollReveal.js
@@ -0,0 +1,15 @@
+
+// geral
+window.sr = ScrollReveal({ 
+    resete: true, 
+    duration: 2500,
+    distance:'60px',
+    
+});
+
+sr.reveal('header', {
+    delay: 900,
+    origin: 'top',
+    duration: 1000,
+});
+
