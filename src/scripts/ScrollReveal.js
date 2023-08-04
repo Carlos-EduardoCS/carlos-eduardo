571cd3e7ecf7cf592a37e8674ae0b5df92fa71be
Carlos Eduardo <2005carloseduardosantos@gmail.com>
2023-08-04 12:24:44 -0300

    2nd comit

--- src/scripts/ScrollReveal.js
+++ src/scripts/ScrollReveal.js
@@ -7,9 +7,73 @@ window.sr = ScrollReveal({
     
 });
 
-sr.reveal('header', {
+sr.reveal('.carlos', {
     delay: 900,
+    origin: 'left',
+    duration: 1000,
+});
+
+sr.reveal('.line1', {
+    delay: 900,
+    duration: 1000,
+});
+
+sr.reveal('.more-about', {
+    delay: 1000,
+    origin: 'left',
+    duration: 1000,
+});
+
+sr.reveal('.breve-apre', {
+    delay: 1500,
+    origin: 'left',
+    duration: 1000,
+});
+
+sr.reveal('.line2', {
+    delay: 950,
+    origin: 'right',
+    duration: 1000,
+});
+
+sr.reveal('.carlos-memoji', {
+    delay: 950,
+    origin: 'right',
+    duration: 1000,
+});
+
+sr.reveal('button', {
+    delay: 950,
     origin: 'top',
     duration: 1000,
 });
+
+sr.reveal('.card1', {
+    delay: 950,
+    origin: 'top',
+    duration: 900,
+});
+
+sr.reveal('.card2', {
+    delay: 1050,
+    origin: 'top',
+    duration: 900,
+});
+
+sr.reveal('.card3', {
+    delay: 1100,
+    origin: 'top',
+    duration: 900,
+});
+
+sr.reveal('', {
+    delay: 1100,
+    origin: 'top',
+    duration: 1000,
+});
+
+
+
+
+
 
